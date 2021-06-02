ab847666f514ed1657f42706e792135bf9c22b46
Vitor Spessoto <spessoto97@icloud.com>
2021-06-02 19:18:33 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vitor Spessoto on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
