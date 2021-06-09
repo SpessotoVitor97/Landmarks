7c0fcf8f025e2ee3681ef4ad471a357eb9f7b323
Vitor Spessoto <spessoto97@icloud.com>
2021-06-09 19:50:50 -0300

    Building Lists and Navigation

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Vitor Spessoto on 02/06/21.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData")
+
+func load<T: Decodable>(_ filename: String, withExtension: String = ".json") -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: withExtension) else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) in main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
